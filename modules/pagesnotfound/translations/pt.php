78d66503ad7d0bd51b270835017fd7483fd16d6f
marcelo.teixeira <contacto.sexoja@gmail.com>
2014-10-06 14:03:58 +0100

    Initial commit with contributors

---
+++ modules/pagesnotfound/translations/pt.php
@@ -0,0 +1,25 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_251295238bdf7693252f2804c8d3707e'] = 'Páginas não encontradas';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_607cc8b8993662a37cac86032fb071d2'] = 'Adiciona um painel de estatísticas, apresentando as páginas requisitadas mas não encontradas pelos visitantes.';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_dc3a3db6b98723bf91f924537a630600'] = 'A cache das \\"Páginas não encontradas\\" foi limpa.';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_b323790d8ee3c43d317d19aea5012626'] = 'A cache "páginas não encontradas" foram eliminadas.';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_6602bbeb2956c035fb4cb5e844a4861b'] = 'Guia';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_3604249130acf7fda296e16edc996e5b'] = 'erros 404';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_a90083861c168ef985bf70763980aa60'] = 'Como capturar estes erros?';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_4f803d59ee120b11662027e049cba1f3'] = 'Se o seu serviço de hospedagem suportar ficheiros .htaccess, pode criar um no diretório raiz do PrestaShop e inserir a seguinte linha: "%s".';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_07e7f83ae625fe216a644d09feab4573'] = 'Um utilizador que tenha requisitado uma página que não existe será redirecionado para a seguinte página: %s. Este módulo regista o acesso a esta página.';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_01bd0bf7c5a68ad6ee4423118be3f7b6'] = 'Tem de usar um ficheiro .htaccess para redirecionar erros 404 para a página "404.php".';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_193cfc9be3b995831c6af2fea6650e60'] = 'Página';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_b6f05e5ddde1ec63d992d61144452dfa'] = 'Página de referência';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_64d129224a5377b63e9727479ec987d9'] = 'Contador';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_4a7a7e7cda40454cee7ec247660f8017'] = 'Nenhum evento "página não encontrada" foi registado por enquanto.';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_d8847bc418fc4f5a3e37c2e8390bb9ed'] = 'Limpar a base de dados';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_b9ae3636d6e672413a163f7cb34beb84'] = 'Eliminar todas as notificações "páginas não encontradas" para este período';
+$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_0cf5c3a279c0e8c57b232d8c6bc3f06a'] = 'Eliminar todas as notificações "páginas não encontradas"';
+
+
+return $_MODULE;
---
+++ modules/statsequipment/translations/pt.php
@@ -0,0 +1,13 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+
+$_MODULE['<{statsequipment}prestashop>statsequipment_247270d410e2b9de01814b82111becda'] = 'Browzer e Sistema operativo';
+$_MODULE['<{statsequipment}prestashop>statsequipment_2876718a648dea03aaafd4b5a63b1efe'] = 'Acrescenta ao painel das estatísticas um separador que contém gráficos sobre os navegadores de internet e sistemas operativos usados.';
+$_MODULE['<{statsequipment}prestashop>statsequipment_6602bbeb2956c035fb4cb5e844a4861b'] = 'Guia';
+$_MODULE['<{statsequipment}prestashop>statsequipment_998e4c5c80f27dec552e99dfed34889a'] = 'Exportar para CSV';
+$_MODULE['<{statsequipment}prestashop>statsequipment_bb38096ab39160dc20d44f3ea6b44507'] = 'Plug-ins';
+
+
+return $_MODULE;
